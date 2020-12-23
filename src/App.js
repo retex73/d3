983f84fb182f64ed65936ad8b7f084f89875cc10
Peter Williams <retex.williams@gmail.com>
2020-12-23 17:17:05 +0000

    curved line chart

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React, { useRef, useEffect, useState } from 'react'
 import './App.css';
-import { select } from 'd3'
-
-// const data = [25, 30, 45, 60, 20, 35]
+import { select, line, curveCardinal } from 'd3'
 
 function App() {
-  const [data, setData] = useState([25, 30, 45, 60, 20, 35])
+  const [data, setData] = useState([25, 30, 45, 60, 20, 65, 75])
   const svgRef = useRef()
   useEffect(() => {
     const svg = select(svgRef.current)
+    const myLine = line()
+      .x((value, index) => index * 50)
+      .y(value => 150 - value)
+      .curve(curveCardinal)
+
     svg
-      .selectAll('circle')
-      .data(data)
-      .join("circle")
-      .attr('r', value => value)
-      .attr('cx', value => value * 2)
-      .attr('cy', value => value * 2)
-      .attr('stroke', 'green')
-      .attr('fill', 'white')
+      .selectAll("path")
+      .data([data])
+      .join("path")
+      .attr("d", value => myLine(value))
+      .attr("stroke", "blue")
+      .attr("fill", "none")
+
   }, [data])
   return (
     <React.Fragment>
